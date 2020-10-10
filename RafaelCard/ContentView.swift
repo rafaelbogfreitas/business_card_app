6a42afb282e82cb1271d043958f752ee6c27eebf
Rafael Freitas <rafaelborges1985@hotmail.com>
2020-10-10 15:00:42 -0300

    Initial commit

---
+++ RafaelCard/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  RafaelCard
+//
+//  Created by Rafael Freitas on 10/10/20.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ RafaelCard/RafaelCardApp.swift
@@ -0,0 +1,17 @@
+//
+//  RafaelCardApp.swift
+//  RafaelCard
+//
+//  Created by Rafael Freitas on 10/10/20.
+//
+
+import SwiftUI
+
+@main
+struct RafaelCardApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
