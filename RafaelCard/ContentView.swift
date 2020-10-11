1b2b7e9835fdf0f0d5b4772996f9895aeb164192
Rafael Freitas <rafaelborges1985@hotmail.com>
2020-10-10 23:35:53 -0300

    Create UI and add Thin font for subtitle

--- RafaelCard/ContentView.swift
+++ RafaelCard/ContentView.swift
@@ -9,10 +9,72 @@ import SwiftUI
 
 struct ContentView: View {
     var body: some View {
-        Text("Hello, world!")
+        ZStack {
+            Color(.white)
+                .edgesIgnoringSafeArea(.all)
+            VStack(alignment: .center, spacing: 20) {
+                
+                Image("rafael")
+                    .resizable()
+                    .aspectRatio(contentMode: .fit)
+                    .frame(width: 100, height: 100, alignment: .center)
+                    .clipShape(Circle())
+                    .overlay(Circle().stroke(lineWidth: 3))
+                
+                VStack {
+                    Text("RAFAEL")
+                        .foregroundColor(.white)
+                        .frame(width: .infinity)
+                    
+                    Text("FREITAS")
+                        .frame(width: .infinity)
+                        .foregroundColor(customOrange)
+                        .background(Color.white)
+                }
+                .background(customOrange)
+                .font(Font.custom("Roboto-Bold", size: 40))
+                
+                Text("Desenvolvedor iOS")
+                    .font(Font.custom("Roboto-Thin", size: 15))
+                
+                Divider()
+               
+                info(for: "rafael@freitas.com", icon: "envelope.fill")
+                info(for: "0748151908", icon: "phone.fill")
+                
+            }
             .padding()
+            .foregroundColor(customOrange)
+        }
     }
+    
+    func info(for field: String, icon: String) -> some View {
+        ZStack {
+            RoundedRectangle(cornerRadius: 20)
+                .frame(height: 40)
+            HStack {
+                Image(systemName: icon)
+                Text(field)
+                    .font(Font.custom("Roboto-Bold", size: 15))
+            }
+            .foregroundColor(.white)
+        }
+    }
+    
+    //MARK: - Constants
+    let customOrange = Color(.orange)
 }
+
+
+
+
+
+
+
+
+
+
+
 
 struct ContentView_Previews: PreviewProvider {
     static var previews: some View {
